c418fc95d7e4e10bddc7a4452c78cf6d14160ae2
raed guembri <60182483+raed717@users.noreply.github.com>
2023-02-16 11:49:18 +0100

    first push

--- src/main/java/com/example/geekslab/Entite/Question.java
+++ src/main/java/com/example/geekslab/Entite/Question.java
@@ -2,5 +2,42 @@ package com.example.geekslab.Entite;
 
 import java.io.Serializable;
 
-public class Question implements Serializable {
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+import lombok.*;
+import lombok.experimental.FieldDefaults;
+import javax.persistence.*;
+import java.io.Serializable;
+import java.util.ArrayList;
+import java.util.Collection;
+import java.util.List;
+@AllArgsConstructor
+@NoArgsConstructor
+@Getter
+@Setter
+@ToString
+@Entity
+public class Question {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private String description;
+
+    @ManyToOne
+    private Quiz quiz;
+    @JsonIgnoreProperties("question")
+    @OneToMany(mappedBy = "question", cascade = CascadeType.ALL)
+    private List<Answer> answers;
+
+
+    public void addAnswer(Answer answer) {
+        if (answers == null) {
+            answers = new ArrayList<>();
+        }
+        answers.add(answer);
+        answer.setQuestion(this);
+    }
+
+
 }
