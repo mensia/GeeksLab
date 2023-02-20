c4c3bf1610250429e518e0258ef1e72a723dc82c
ihebsaidi98 <99597374+ihebsaidi98@users.noreply.github.com>
2023-02-20 17:41:01 +0100

    push

--- src/main/java/com/example/geekslab/Entite/Call.java
+++ src/main/java/com/example/geekslab/Entite/Call.java
@@ -1,9 +1,21 @@
 package com.example.geekslab.Entite;
 
-import java.io.Serializable;
+import lombok.*;
+import lombok.experimental.FieldDefaults;
 
+import javax.persistence.Entity;
+import java.io.Serializable;
+@Entity
+@AllArgsConstructor
+@NoArgsConstructor
+@Getter
+@Setter
+@ToString
+@FieldDefaults(level = AccessLevel.PRIVATE)
 public class Call implements Serializable {
     // chaque fournisseur propose une offre a la demande du ChefService
     // le hcef accepte l offre 'call' qui lui plait
     // une notification est envoyer au fournissur
+
+
 }
--- src/main/java/com/example/geekslab/Entite/Material.java
+++ src/main/java/com/example/geekslab/Entite/Material.java
@@ -1,6 +1,16 @@
 package com.example.geekslab.Entite;
 
-import java.io.Serializable;
+import lombok.*;
+import lombok.experimental.FieldDefaults;
 
+import javax.persistence.Entity;
+import java.io.Serializable;
+@Entity
+@AllArgsConstructor
+@NoArgsConstructor
+@Getter
+@Setter
+@ToString
+@FieldDefaults(level = AccessLevel.PRIVATE)
 public class Material implements Serializable {
 }
