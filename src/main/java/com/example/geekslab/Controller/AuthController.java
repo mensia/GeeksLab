95d80c532aeb805c139e61828b1f75b3a399a503
mensia <hana.mensia@esprit.tn>
2023-02-21 16:38:05 +0100

    no security

--- src/main/java/com/example/geekslab/Controller/AuthController.java
+++ src/main/java/com/example/geekslab/Controller/AuthController.java
@@ -1,4 +1,18 @@
 package com.example.geekslab.Controller;
 
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
 public class AuthController {
+   @GetMapping("/home")
+    public String Home () {
+        return "this is home page " ;
+    }
+
+    @GetMapping("/admin")
+    public String Admin () {
+        return "this is Admin page " ;
+    }
+
 }
--- src/main/java/com/example/geekslab/Security/SecurityConstants.java
+++
@@ -1,4 +0,0 @@
-package com.example.geekslab.Security;
-
-public class SecurityConstants {
-}
